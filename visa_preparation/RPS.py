663d0b2bf9c6d61750eac4c778c42f41198ee31a
shreejabommakanti <136147746+shreejabommakanti@users.noreply.github.com>
2024-11-15 18:08:05 +0530

    Create RPS.py

---
+++ visa_preparation/RPS.py
@@ -0,0 +1,13 @@
+v,c = input().split()
+if v == 'R' and c == 'P':
+    print("Charan")
+elif v == 'P' and c == 'S':
+    print("Charan")
+elif v == 'S' and c == 'R':
+    print("Charan")
+elif v == 'P' and c == 'S':
+    print("Charan")
+elif v == c:
+    print("NULL")
+else:
+    print("Vignesh")
